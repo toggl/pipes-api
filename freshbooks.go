e5f0d85e3cf728bcb69a47cf0f7c9a80d9230f72
Tambet Masik <tambet@toggl.com>
2014-05-20 15:33:24 +0300

    Add users to Freshbooks service

--- freshbooks.go
+++ freshbooks.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/toggl/go-freshbooks"
+	"os"
 )
 
 type FreshbooksService struct {
@@ -34,7 +36,23 @@ func (s *FreshbooksService) Accounts() ([]*Account, error) {
 }
 
 func (s *FreshbooksService) Users() ([]*User, error) {
-	return nil, nil
+	account := os.Getenv("FRESHBOOKS_ACCOUNT")
+	token := os.Getenv("FRESHBOOKS_TOKEN")
+	apiClient := freshbooks.NewApi(account, token)
+	foreignObjects, err := apiClient.Users()
+	if err != nil {
+		return nil, err
+	}
+	var users []*User
+	for _, object := range foreignObjects {
+		user := User{
+			ForeignID: object.UserId,
+			Name:      fmt.Sprintf("%s %s", object.FirstName, object.LastName),
+			Email:     object.Email,
+		}
+		users = append(users, &user)
+	}
+	return users, nil
 }
 
 func (s *FreshbooksService) Projects() ([]*Project, error) {
