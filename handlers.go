a55e1efa4bd7bd220bbad277fd769636c8c689e9
Tambet Masik <tambet@toggl.com>
2014-05-29 11:02:44 +0300

    Use anonymous struct for DB status

--- handlers.go
+++ handlers.go
@@ -252,17 +252,13 @@ func postPipeRun(req Request) Response {
 	return ok(nil)
 }
 
-type (
-	statusResponse struct {
-		Reasons []string `json:"reasons"`
-	}
-)
-
 func getStatus(req Request) Response {
 	if dbIsDown() {
-		resp := &statusResponse{}
-		resp.Reasons = make([]string, 1)
-		resp.Reasons[0] = "Database is down"
+		resp := &struct {
+			Reasons []string `json:"reasons"`
+		}{
+			[]string{"Database is down"},
+		}
 		return serviceUnavailable(resp)
 	}
 	return ok("OK")
