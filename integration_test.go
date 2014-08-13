7066a185da2b02ddff86a54882aa222092bdfb19
Tambet Masik <tambet@toggl.com>
2014-08-13 12:14:42 +0300

    Integration pipes test added

--- integration_test.go
+++ integration_test.go
@@ -1,35 +1,78 @@
 package main
 
 import (
-  "reflect"
+	"reflect"
 	"testing"
 )
 
 var testDB = "pipes_test"
 
 func init() {
-  db = connectDB(*dbHost, *dbPort, testDB, *dbUser, *dbPass)
+	loadIntegrations()
+	db = connectDB(*dbHost, *dbPort, testDB, *dbUser, *dbPass)
 }
 
 func TestWorkspaceIntegrations(t *testing.T) {
 	integrations, err := workspaceIntegrations(workspaceID)
 
-  if err != nil {
-    t.Fatalf("workspaceIntegrations returned error: %v", err)
-  }
+	if err != nil {
+		t.Fatalf("workspaceIntegrations returned error: %v", err)
+	}
 
   for i, _ := range integrations {
 	  integrations[i].Pipes = nil
-  }
+	}
 
-  want := []Integration{
-    {ID: "basecamp", Name: "Basecamp", Link: "http://support.toggl.com/basecamp", Image:"/images/logo-basecamp.png", AuthType:"oauth2"},
-    {ID: "freshbooks", Name: "Freshbooks", Link: "http://support.toggl.com/freshbooks", Image:"/images/logo-freshbooks.png", AuthType:"oauth1"},
-    {ID: "teamweek", Name: "Teamweek", Link: "http://support.toggl.com/teamweek", Image:"/images/logo-teamweek.png", AuthType:"oauth2"},
-    {ID: "asana", Name: "Asana", Link: "http://support.toggl.com/asana", Image:"/images/logo-asana.png", AuthType:"oauth2"},
-  }
+	want := []Integration{
+		{ID: "basecamp", Name: "Basecamp", Link: "http://support.toggl.com/basecamp", Image: "/images/logo-basecamp.png", AuthType: "oauth2"},
+		{ID: "freshbooks", Name: "Freshbooks", Link: "http://support.toggl.com/freshbooks", Image: "/images/logo-freshbooks.png", AuthType: "oauth1"},
+		{ID: "teamweek", Name: "Teamweek", Link: "http://support.toggl.com/teamweek", Image: "/images/logo-teamweek.png", AuthType: "oauth2"},
+		{ID: "asana", Name: "Asana", Link: "http://support.toggl.com/asana", Image: "/images/logo-asana.png", AuthType: "oauth2"},
+	}
 
-  if !reflect.DeepEqual(want, integrations) {
-    t.Errorf("workspaceIntegrations returned %+v, want %+v", integrations, want)
+	if !reflect.DeepEqual(want, integrations) {
+		t.Errorf("workspaceIntegrations returned %+v, want %+v", integrations, want)
+	}
+}
+
+func TestWorkspaceIntegrationPipes(t *testing.T) {
+	integrations, err := workspaceIntegrations(workspaceID)
+
+	if err != nil {
+		t.Fatalf("workspaceIntegrations returned error: %v", err)
+	}
+
+	want := [][]*Pipe{
+    { // Basecamp
+	    {ID: "users", Name: "Users", Premium: false, AutomaticOption: false},
+	    {ID: "projects", Name: "Projects", Premium: false, AutomaticOption: true},
+	    {ID: "todolists", Name: "Todo lists", Premium: true, AutomaticOption: true},
+	    {ID: "todos", Name: "Todos", Premium: true, AutomaticOption: true},
+    },
+    { // Freshbooks
+	    {ID: "users", Name: "Users", Premium: false, AutomaticOption: false},
+	    {ID: "projects", Name: "Projects", Premium: false, AutomaticOption: true},
+	    {ID: "tasks", Name: "Tasks", Premium: true, AutomaticOption: true},
+	    {ID: "timeentries", Name: "Time entries", Premium: true, AutomaticOption: true},
+    },
+    { // Teamweek
+	    {ID: "users", Name: "Users", Premium: false, AutomaticOption: false},
+	    {ID: "projects", Name: "Projects", Premium: false, AutomaticOption: true},
+	    {ID: "tasks", Name: "Tasks", Premium: true, AutomaticOption: true},
+    },
+    { // Asana
+	    {ID: "users", Name: "Users", Premium: false, AutomaticOption: false},
+	    {ID: "projects", Name: "Projects", Premium: false, AutomaticOption: true},
+	    {ID: "tasks", Name: "Tasks", Premium: true, AutomaticOption: true},
+    },
+	}
+
+  for i, _ := range integrations {
+    for j, pipe := range integrations[i].Pipes {
+      pipe.Description = ""
+      if !reflect.DeepEqual(pipe, want[i][j]) {
+        t.Errorf("workspaceIntegrations returned %+v, want %+v", pipe, want[i][j])
+      }
+    }
   }
 }
