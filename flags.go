f0287bf7f8ff035fd1921cb1a28167d2cdf4b177
Radim Marek <radim@laststation.net>
2017-01-30 14:00:10 +0100

    missing parse

--- flags.go
+++ flags.go
@@ -21,4 +21,6 @@ func InitFlags() {
 	fs.StringVar(&bugsnagAPIKey, "bugsnag_key", "", "Bugsnag API Key")
 	fs.StringVar(&environment, "environment", "development", "Environment")
 	fs.StringVar(&dbConnString, "db_conn_string", "dbname=pipes_development user=pipes_user host=localhost sslmode=disable port=5432", "DB Connection String")
+
+	fs.Parse(os.Args[1:])
 }
