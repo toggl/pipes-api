5f2b21c680377a0161864029fdf4145929c6934b
Anton Kucherov <anton.kucherov@toggl.com>
2020-03-04 14:16:10 +0300

    Add integrations tests.

--- pkg/integrations/integration_test.go
+++ pkg/integrations/integration_test.go
@@ -1,136 +1,32 @@
 package integrations
 
 import (
-	"database/sql"
-	"log"
-	"reflect"
 	"testing"
 
-	"github.com/toggl/pipes-api/pkg/authorization"
-	"github.com/toggl/pipes-api/pkg/config"
-	"github.com/toggl/pipes-api/pkg/connection"
-	"github.com/toggl/pipes-api/pkg/toggl"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/toggl/pipes-api/pkg/integrations/asana"
+	"github.com/toggl/pipes-api/pkg/integrations/basecamp"
+	"github.com/toggl/pipes-api/pkg/integrations/freshbooks"
+	"github.com/toggl/pipes-api/pkg/integrations/github"
+	"github.com/toggl/pipes-api/pkg/integrations/teamweek"
 )
 
-func TestWorkspaceIntegrations(t *testing.T) {
-	t.Skipf("DEPRECATED TEST: Should be removed after new will be created")
+func TestNewExternalService(t *testing.T) {
+	s1 := NewExternalService(basecamp.ServiceID, 1)
+	s2 := NewExternalService(asana.ServiceID, 2)
+	s3 := NewExternalService(github.ServiceID, 3)
+	s4 := NewExternalService(freshbooks.ServiceID, 4)
+	s5 := NewExternalService(teamweek.ServiceID, 5)
 
-	flags := config.Flags{}
-	config.ParseFlags(&flags)
-
-	cfg := config.Load(flags.Environment, flags.WorkDir)
-	togglApiHost := cfg.Urls.TogglAPIHost[cfg.EnvType]
-	pipesApiHost := cfg.Urls.PipesAPIHost[cfg.EnvType]
-
-	db, err := sql.Open("postgres", flags.DbConnString)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	api := toggl.NewApiClient(togglApiHost)
-	authStore := authorization.NewStorage(db, cfg)
-	connStore := connection.NewStorage(db)
-
-	pipesStorage := NewStorage(db)
-	pipeService := NewService(cfg, authStore, pipesStorage, connStore, api, pipesApiHost, cfg.WorkDir)
-
-	integrations, err := pipeService.WorkspaceIntegrations(workspaceID)
-
-	if err != nil {
-		t.Fatalf("workspaceIntegrations returned error: %v", err)
-	}
-
-	for i := range integrations {
-		integrations[i].Pipes = nil
-	}
-
-	want := []Integration{
-		{ID: "basecamp", Name: "Basecamp", Link: "https://support.toggl.com/import-and-export/integrations-via-toggl-pipes/integration-with-basecamp", Image: "/images/logo-basecamp.png", AuthType: "oauth2"},
-		{ID: "freshbooks", Name: "Freshbooks", Link: "https://support.toggl.com/import-and-export/integrations-via-toggl-pipes/integration-with-freshbooks-classic", Image: "/images/logo-freshbooks.png", AuthType: "oauth1"},
-		{ID: "teamweek", Name: "Toggl Plan", Link: "https://support.toggl.com/en/articles/2212490-integration-with-toggl-plan-teamweek", Image: "/images/logo-teamweek.png", AuthType: "oauth2"},
-		{ID: "asana", Name: "Asana", Link: "https://support.toggl.com/import-and-export/integrations-via-toggl-pipes/integration-with-asana", Image: "/images/logo-asana.png", AuthType: "oauth2"},
-		{ID: "github", Name: "Github", Link: "https://support.toggl.com/import-and-export/integrations-via-toggl-pipes/integration-with-github", Image: "/images/logo-github.png", AuthType: "oauth2"},
-	}
-
-	if len(integrations) != len(want) {
-		t.Fatalf("Load integration(s) detected - please add tests!")
-	}
-
-	for i := range integrations {
-		if !reflect.DeepEqual(integrations[i], want[i]) {
-			t.Fatalf("workspaceIntegrations returned  ---------->\n%+v, \nwant ---------->\n%+v", integrations[i], want[i])
-		}
-	}
+	assert.Equal(t, basecamp.ServiceID, s1.ID())
+	assert.Equal(t, asana.ServiceID, s2.ID())
+	assert.Equal(t, github.ServiceID, s3.ID())
+	assert.Equal(t, freshbooks.ServiceID, s4.ID())
+	assert.Equal(t, teamweek.ServiceID, s5.ID())
 }
 
-func TestWorkspaceIntegrationPipes(t *testing.T) {
-	t.Skipf("DEPRECATED TEST: Should be removed after new will be created")
-
-	flags := config.Flags{}
-	config.ParseFlags(&flags)
-
-	cfg := config.Load(flags.Environment, flags.WorkDir)
-	togglApiHost := cfg.Urls.TogglAPIHost[cfg.EnvType]
-	pipesApiHost := cfg.Urls.PipesAPIHost[cfg.EnvType]
-
-	db, err := sql.Open("postgres", flags.DbConnString)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	api := toggl.NewApiClient(togglApiHost)
-	authStore := authorization.NewStorage(db, cfg)
-	connStore := connection.NewStorage(db)
-
-	pipesStorage := NewStorage(db)
-	pipeService := NewService(cfg, authStore, pipesStorage, connStore, api, pipesApiHost, cfg.WorkDir)
-
-	integrations, err := pipeService.WorkspaceIntegrations(workspaceID)
-
-	if err != nil {
-		t.Fatalf("workspaceIntegrations returned error: %v", err)
-	}
-
-	want := [][]*Pipe{
-		{ // Basecamp
-			{ID: "users", Name: "Users", Premium: false, AutomaticOption: false},
-			{ID: "projects", Name: "Projects", Premium: false, AutomaticOption: true},
-			{ID: "todolists", Name: "Todo lists", Premium: true, AutomaticOption: true},
-			{ID: "todos", Name: "Todos", Premium: true, AutomaticOption: true},
-		},
-		{ // Freshbooks
-			{ID: "users", Name: "Users", Premium: false, AutomaticOption: false},
-			{ID: "projects", Name: "Projects", Premium: false, AutomaticOption: true},
-			{ID: "tasks", Name: "Tasks", Premium: true, AutomaticOption: true},
-			{ID: "timeentries", Name: "Time entries", Premium: true, AutomaticOption: true},
-		},
-		{ // Teamweek
-			{ID: "users", Name: "Users", Premium: false, AutomaticOption: false},
-			{ID: "projects", Name: "Projects", Premium: false, AutomaticOption: true},
-			{ID: "tasks", Name: "Tasks", Premium: true, AutomaticOption: true},
-		},
-		{ // Asana
-			{ID: "users", Name: "Users", Premium: false, AutomaticOption: false},
-			{ID: "projects", Name: "Projects", Premium: false, AutomaticOption: true},
-			{ID: "tasks", Name: "Tasks", Premium: true, AutomaticOption: true},
-		},
-		{ // Github
-			{ID: "projects", Name: "Github repos", Premium: false, AutomaticOption: true},
-		},
-	}
-
-	if len(integrations) != len(want) {
-		t.Fatalf("Load integration(s) detected - please add tests!")
-	}
-
-	for i := range integrations {
-		for j, pipe := range integrations[i].Pipes {
-			pipe.Description = ""
-			if !reflect.DeepEqual(pipe, want[i][j]) {
-				t.Fatalf("workspaceIntegrations returned  ---------->\n%+v, \nwant ---------->\n%+v", pipe, want[i][j])
-			}
-		}
-	}
+func TestNewExternalServicePanic(t *testing.T) {
+	pf := func() { NewExternalService("Unknown", 1) }
+	assert.Panics(t, pf)
 }
--- pkg/integrations/service_test.go
+++ pkg/integrations/service_test.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"log"
+	"reflect"
 	"testing"
 
 	"github.com/toggl/pipes-api/pkg/authorization"
@@ -121,5 +122,128 @@ func TestGetPipesFromQueue_DoesNotReturnMultipleSameWorkspace(t *testing.T) {
 	}
 	if _, exists := retrievedWorkspace[pipes[0].WorkspaceID]; !exists {
 		t.Error("should return pipe with workspace from retrievedWorkspace")
+	}
+}
+
+func TestWorkspaceIntegrations(t *testing.T) {
+	t.Skipf("DEPRECATED TEST: Should be removed after new will be created")
+
+	flags := config.Flags{}
+	config.ParseFlags(&flags)
+
+	cfg := config.Load(flags.Environment, flags.WorkDir)
+	togglApiHost := cfg.Urls.TogglAPIHost[cfg.EnvType]
+	pipesApiHost := cfg.Urls.PipesAPIHost[cfg.EnvType]
+
+	db, err := sql.Open("postgres", flags.DbConnString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	api := toggl.NewApiClient(togglApiHost)
+	authStore := authorization.NewStorage(db, cfg)
+	connStore := connection.NewStorage(db)
+
+	pipesStorage := NewStorage(db)
+	pipeService := NewService(cfg, authStore, pipesStorage, connStore, api, pipesApiHost, cfg.WorkDir)
+
+	integrations, err := pipeService.WorkspaceIntegrations(workspaceID)
+
+	if err != nil {
+		t.Fatalf("workspaceIntegrations returned error: %v", err)
+	}
+
+	for i := range integrations {
+		integrations[i].Pipes = nil
+	}
+
+	want := []Integration{
+		{ID: "basecamp", Name: "Basecamp", Link: "https://support.toggl.com/import-and-export/integrations-via-toggl-pipes/integration-with-basecamp", Image: "/images/logo-basecamp.png", AuthType: "oauth2"},
+		{ID: "freshbooks", Name: "Freshbooks", Link: "https://support.toggl.com/import-and-export/integrations-via-toggl-pipes/integration-with-freshbooks-classic", Image: "/images/logo-freshbooks.png", AuthType: "oauth1"},
+		{ID: "teamweek", Name: "Toggl Plan", Link: "https://support.toggl.com/en/articles/2212490-integration-with-toggl-plan-teamweek", Image: "/images/logo-teamweek.png", AuthType: "oauth2"},
+		{ID: "asana", Name: "Asana", Link: "https://support.toggl.com/import-and-export/integrations-via-toggl-pipes/integration-with-asana", Image: "/images/logo-asana.png", AuthType: "oauth2"},
+		{ID: "github", Name: "Github", Link: "https://support.toggl.com/import-and-export/integrations-via-toggl-pipes/integration-with-github", Image: "/images/logo-github.png", AuthType: "oauth2"},
+	}
+
+	if len(integrations) != len(want) {
+		t.Fatalf("Load integration(s) detected - please add tests!")
+	}
+
+	for i := range integrations {
+		if !reflect.DeepEqual(integrations[i], want[i]) {
+			t.Fatalf("workspaceIntegrations returned  ---------->\n%+v, \nwant ---------->\n%+v", integrations[i], want[i])
+		}
+	}
+}
+
+func TestWorkspaceIntegrationPipes(t *testing.T) {
+	t.Skipf("DEPRECATED TEST: Should be removed after new will be created")
+
+	flags := config.Flags{}
+	config.ParseFlags(&flags)
+
+	cfg := config.Load(flags.Environment, flags.WorkDir)
+	togglApiHost := cfg.Urls.TogglAPIHost[cfg.EnvType]
+	pipesApiHost := cfg.Urls.PipesAPIHost[cfg.EnvType]
+
+	db, err := sql.Open("postgres", flags.DbConnString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	api := toggl.NewApiClient(togglApiHost)
+	authStore := authorization.NewStorage(db, cfg)
+	connStore := connection.NewStorage(db)
+
+	pipesStorage := NewStorage(db)
+	pipeService := NewService(cfg, authStore, pipesStorage, connStore, api, pipesApiHost, cfg.WorkDir)
+
+	integrations, err := pipeService.WorkspaceIntegrations(workspaceID)
+
+	if err != nil {
+		t.Fatalf("workspaceIntegrations returned error: %v", err)
+	}
+
+	want := [][]*Pipe{
+		{ // Basecamp
+			{ID: "users", Name: "Users", Premium: false, AutomaticOption: false},
+			{ID: "projects", Name: "Projects", Premium: false, AutomaticOption: true},
+			{ID: "todolists", Name: "Todo lists", Premium: true, AutomaticOption: true},
+			{ID: "todos", Name: "Todos", Premium: true, AutomaticOption: true},
+		},
+		{ // Freshbooks
+			{ID: "users", Name: "Users", Premium: false, AutomaticOption: false},
+			{ID: "projects", Name: "Projects", Premium: false, AutomaticOption: true},
+			{ID: "tasks", Name: "Tasks", Premium: true, AutomaticOption: true},
+			{ID: "timeentries", Name: "Time entries", Premium: true, AutomaticOption: true},
+		},
+		{ // Teamweek
+			{ID: "users", Name: "Users", Premium: false, AutomaticOption: false},
+			{ID: "projects", Name: "Projects", Premium: false, AutomaticOption: true},
+			{ID: "tasks", Name: "Tasks", Premium: true, AutomaticOption: true},
+		},
+		{ // Asana
+			{ID: "users", Name: "Users", Premium: false, AutomaticOption: false},
+			{ID: "projects", Name: "Projects", Premium: false, AutomaticOption: true},
+			{ID: "tasks", Name: "Tasks", Premium: true, AutomaticOption: true},
+		},
+		{ // Github
+			{ID: "projects", Name: "Github repos", Premium: false, AutomaticOption: true},
+		},
+	}
+
+	if len(integrations) != len(want) {
+		t.Fatalf("Load integration(s) detected - please add tests!")
+	}
+
+	for i := range integrations {
+		for j, pipe := range integrations[i].Pipes {
+			pipe.Description = ""
+			if !reflect.DeepEqual(pipe, want[i][j]) {
+				t.Fatalf("workspaceIntegrations returned  ---------->\n%+v, \nwant ---------->\n%+v", pipe, want[i][j])
+			}
+		}
 	}
 }
