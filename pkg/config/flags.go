b7b92d915073375cc7093daf0fed1c3d468d1925
Anton Kucherov <anton.kucherov@toggl.com>
2020-03-24 18:42:03 +0300

    Fi typo

--- pkg/config/flags.go
+++ pkg/config/flags.go
@@ -28,7 +28,7 @@ func ParseFlags(flags *Flags, args []string) {
 	fs.IntVar(&flags.Port, "port", 8100, "port")
 	fs.StringVar(&flags.WorkDir, "workdir", ".", "Workdir of server")
 	fs.StringVar(&flags.BugsnagAPIKey, "bugsnag_key", "", "Bugsnag API Key")
-	fs.StringVar(&flags.Environment, "EnvType", EnvTypeDevelopment, "env")
+	fs.StringVar(&flags.Environment, "environment", EnvTypeDevelopment, "Current environment")
 	fs.StringVar(&flags.DbConnString, "db_conn_string", "dbname=pipes_development user=pipes_user host=localhost sslmode=disable port=5432", "DB Connection String")
 
 	fs.Parse(args[1:])
